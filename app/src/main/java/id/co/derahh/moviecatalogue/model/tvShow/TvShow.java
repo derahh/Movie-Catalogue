12e444a47ce6c6be39a19265437993813e2ff9df
Dedi Rahmansah <derah666@gmail.com>
2020-01-24 14:10:34 +0700

    Update TvShow.java

--- app/src/main/java/id/co/derahh/moviecatalogue/model/tvShow/TvShow.java
+++ app/src/main/java/id/co/derahh/moviecatalogue/model/tvShow/TvShow.java
@@ -4,6 +4,9 @@ import android.database.Cursor;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import com.google.gson.annotations.SerializedName;
 
 import org.json.JSONObject;
@@ -15,9 +18,11 @@ import static id.co.derahh.moviecatalogue.database.DatabaseContract.getColumnDou
 import static id.co.derahh.moviecatalogue.database.DatabaseContract.getColumnInt;
 import static id.co.derahh.moviecatalogue.database.DatabaseContract.getColumnString;
 
+@Entity(tableName = "tv_show_table")
 public class TvShow implements Parcelable {
 
     @SerializedName("id")
+    @PrimaryKey
     private int id;
     @SerializedName("vote_average")
     private double userScore;
@@ -31,27 +36,6 @@ public class TvShow implements Parcelable {
     private String posterPath;
 
     private String photo;
-
-    public TvShow(JSONObject currentMovie) {
-        try {
-            int id = currentMovie.getInt("id");
-            double userScore = currentMovie.getDouble("vote_average");
-            String title = currentMovie.getString("name");
-            String year = currentMovie.getString("first_air_date");
-            String description = currentMovie.getString("overview");
-            String posterPath = currentMovie.getString("poster_path");
-            String photo = "https://image.tmdb.org/t/p/w342/" + posterPath;
-
-            this.id = id;
-            this.userScore = userScore;
-            this.title = title;
-            this.year = year;
-            this.description = description;
-            this.photo = photo;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     public TvShow(Cursor cursor){
         this.id = getColumnInt(cursor, _ID);
