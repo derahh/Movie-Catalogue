d7f3d71750cdf12fe88716814ac75df254899e56
Dedi Rahmansah <derah666@gmail.com>
2020-01-28 11:57:29 +0700

    Create TvShowResult.java

---
+++ app/src/main/java/id/co/derahh/moviecatalogue/database/model/tvShow/TvShowResult.java
@@ -0,0 +1,12 @@
+package id.co.derahh.moviecatalogue.database.model.tvShow;
+
+import java.util.List;
+
+public class TvShowResult {
+
+    private List<TvShow> results;
+
+    public List<TvShow> getResults() {
+        return results;
+    }
+}
