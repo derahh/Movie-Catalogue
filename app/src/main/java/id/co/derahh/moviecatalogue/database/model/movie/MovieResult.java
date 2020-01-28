5abf91633efcfe346de036b98133e5fb37a35086
Dedi Rahmansah <derah666@gmail.com>
2020-01-28 11:57:20 +0700

    Create MovieResult.java

---
+++ app/src/main/java/id/co/derahh/moviecatalogue/database/model/movie/MovieResult.java
@@ -0,0 +1,12 @@
+package id.co.derahh.moviecatalogue.database.model.movie;
+
+import java.util.List;
+
+public class MovieResult {
+
+    private List<Movie> results;
+
+    public List<Movie> getResults() {
+        return results;
+    }
+}
